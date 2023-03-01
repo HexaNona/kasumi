b2593c7212a5c3426e64b94a64dca2a7a58617c8
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-01 10:39:07 -0500

    feat(requester): :sparkles: allow passing any extra axios config to requester

--- src/requestor/index.ts
+++ src/requestor/index.ts
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosInstance } from "axios";
+import axios, { Axios, AxiosInstance, AxiosRequestConfig } from "axios";
 import { RawResponse } from "../type";
 import { RestError } from "../error";
 
@@ -16,18 +16,18 @@ export default class Rest {
             this.__requestor = provide;
         }
     }
-    async get(endpoint: string, params?: any): Promise<any> {
-        const data: RawResponse = (await this.__requestor.get(endpoint, { params })).data;
+    async get(endpoint: string, params?: any, config?: AxiosRequestConfig): Promise<any> {
+        const data: RawResponse = (await this.__requestor.get(endpoint, { params, ...config })).data;
         if (data.code == 0) return data.data;
         else throw new RestError(data.code, data.message);
     }
-    async post(endpoint: string, body?: any): Promise<any> {
-        const data: RawResponse = (await this.__requestor.post(endpoint, body)).data;
+    async post(endpoint: string, body?: any, config?: AxiosRequestConfig): Promise<any> {
+        const data: RawResponse = (await this.__requestor.post(endpoint, body, config)).data;
         if (data.code == 0) return data.data;
         else throw new RestError(data.code, data.message);
     }
-    async put(endpoint: string, body?: any): Promise<any> {
-        const data: RawResponse = (await this.__requestor.put(endpoint, body)).data;
+    async put(endpoint: string, body?: any, config?: AxiosRequestConfig): Promise<any> {
+        const data: RawResponse = (await this.__requestor.put(endpoint, body, config)).data;
         if (data.code == 0) return data.data;
         else throw new RestError(data.code, data.message);
     }
