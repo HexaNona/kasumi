517f1b1c9a4667eb4f28739dd01eb6c2e2fa7952
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-23 13:22:10 -0400

    feat(API): API.Intimacy

---
+++ src/api/intimacy/index.ts
@@ -0,0 +1,46 @@
+import Rest from "../../requestor";
+import { RawIntimacyIndexResponse } from "./type";
+
+export default class Intimacy {
+    private rest: Rest;
+    constructor(rest: Rest) {
+        this.rest = rest;
+    }
+
+    /**
+     * Get intimacy details of a user
+     * @param userId User ID
+     * @returns Intimacy details
+     */
+    async get(userId: string) {
+        return this.rest.get<RawIntimacyIndexResponse>('/intimacy/index', {
+            user_id: userId
+        })
+    }
+
+    /**
+     * Update intimacy details of a user
+     * @param userId User ID
+     */
+    async update(userId: string, { score, socialInfo, imageId }: {
+        /**
+         * Intimacy score/rating
+         */
+        score?: number,
+        /**
+         * Bio/description of the bot
+         */
+        socialInfo?: string,
+        /**
+         * Protrait image URL
+         */
+        imageId?: string
+    }) {
+        return this.rest.post<void>('/intimacy/update', {
+            score,
+            user_id: userId,
+            social_info: socialInfo,
+            img_id: imageId
+        })
+    }
+}
